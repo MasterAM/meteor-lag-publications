e98601cec281bec5ed4748152af2750519d3132c
Alon Amster <m@ster.am>
2015-09-27 19:37:51 +0300

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased][unreleased]
+### Changed
+- Nothing.
+
+## 1.0.0 - 2015-09-27
+### Added
+- Initial release.
+
+[unreleased]: https://github.com/MasterAM/meteor-lag-publications/compare/v1.0.0...HEAD
---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'alon:lag-publications',
+  summary: 'Adds delay to publications on your development machine.',
+  version: '1.0.0',
+  git: 'https://github.com/MasterAM/meteor-lag-publications',
+  documentation: 'README.md',
+  debugOnly: true
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.1');
+  api.use(['meteorhacks:unblock']);
+  api.use(['alon:lag-base']);
+  api.addFiles([
+    'lib/globals.js',
+    'lib/bootstrap.js'
+  ], 'server');
+  api.export('publicationConfigurator', 'server');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('alon:lag-publications');
+  api.addFiles('tests/server/configs.js', 'server');
+  api.addFiles('tests/server/server-test.js', 'server');
+  api.addFiles('tests/client/client-test.js', 'client');
+});
